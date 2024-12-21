3e3d7e848231c3c32772d6b889f3f27429c1ab0e
AmFearLiath <amfearliath@googlemail.com>
2024-12-21 20:59:43 +0100

    Create auth.lua
    Created: auth.lua

---
+++ auth.lua
@@ -0,0 +1,27 @@
+return {
+  allowedPlayers = {"Metzemoto", "AmFearLiath"},  -- Direkte Erlaubnis für diese Spieler
+  allowedGroups = {"Admins", "Moderators"},  -- Erlaubte Gruppen
+  players = {
+      Metzemoto = {
+          UUID = "123e4567-e89b-12d3-a456-426614174001",
+          PlayerName = "Metzemoto",
+          Moderator = false,
+          Admin = true,
+          Groups = {"Admins"}
+      },
+      AmFearLiath = {
+          UUID = "123e4567-e89b-12d3-a456-426614174000",
+          PlayerName = "AmFearLiath",
+          Moderator = false,
+          Admin = true,
+          Groups = {"Admins"}
+      },
+      Mellabella86 = {
+          UUID = "123e4567-e89b-12d3-a456-426614174002",
+          PlayerName = "Mellabella86",
+          Moderator = false,
+          Admin = false,
+          Groups = {"Players"}
+      }
+  }
+}
